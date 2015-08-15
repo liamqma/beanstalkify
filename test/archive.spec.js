4724350ecea688692404237db96fca5e809e8641
Liam Ma <liam.q.ma@gmail.com>
2015-08-15 21:15:44 +1000

    Add comments

--- test/archive.spec.js
+++ test/archive.spec.js
@@ -1,16 +1,16 @@
 "use strict";
 
-var Archive = require('../archive');
-var filename = __dirname + '/fixture/sso-login-4543cbf.zip';
+var Archive = require('../lib/archive');
+var filename = __dirname + '/fixture/app-name-version.zip';
 
 describe('Archive', function(){
 
     it('should should parse file name', function(){
 
         var archive = new Archive(filename);
-        expect(archive.archiveName).to.equal('sso-login-4543cbf.zip');
-        expect(archive.version).to.equal('4543cbf');
-        expect(archive.appName).to.equal('sso-login');
+        expect(archive.archiveName).to.equal('app-name-version.zip');
+        expect(archive.version).to.equal('version');
+        expect(archive.appName).to.equal('app-name');
 
     });
 
