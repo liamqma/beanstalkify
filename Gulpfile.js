47053fed3d1d2f611a97ac3e0042fe827b46e41d
Liam Ma <liam.ma@mi9.com.au>
2015-08-25 17:51:26 +1000

    coveralls

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,21 +2,38 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var plumber = require('gulp-plumber');
+var istanbul = require('gulp-istanbul');
 var eslint = require('gulp-eslint');
+var coveralls = require('gulp-coveralls');
 
 gulp.task('test', function () {
-    return gulp.src(['./test/*.spec.js'], {
-        read: false
-    }).pipe(plumber()).pipe(mocha({
-        reporter: process.env.MOCHA_REPORTER || 'nyan',
-        require: ['./test/test-helper.js']
-    }));
+
+    gulp.src('./lib/*')
+        .pipe(istanbul({includeUntested: true}))
+        .pipe(istanbul.hookRequire())
+        .on('finish', function () {
+            gulp.src('./test/*.spec.js', {read: false})
+                .pipe(plumber())
+                .pipe(mocha({
+                    reporter: process.env.MOCHA_REPORTER || 'spec',
+                    require: ['./test/test-helper.js']
+                }))
+                .pipe(istanbul.writeReports());
+        });
 });
 
-gulp.task('lint', function() {
+gulp.task('lint', function () {
 
     return gulp.src(['lib/*.js'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failOnError());
+});
+
+gulp.task('coveralls', function () {
+    if (!process.env.CI) {
+        return;
+    }
+    gulp.src('./coverage/lcov.info')
+        .pipe(coveralls());
 });
